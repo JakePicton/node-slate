1ab2b529426446c23638cd1dbbfbf5fd9240bf8f
Jake Picton <jpicton@qvalent.com>
2018-02-23 13:04:42 +1100

    node-slate
    * Changed build to create multiple .html files from .md sources using
    layout.ejs as a template

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,21 @@
-var fs = require('fs');
-
-var gulp = require('gulp');
+var assign = require('lodash.assign');
 var cleanCSS = require('gulp-clean-css');
 var concat = require('gulp-concat');
-var ejs = require('gulp-ejs');
+var del = require('del');
+var frontMatter = require('gulp-front-matter');
+var fs = require('fs');
 var gls = require('gulp-live-server');
+var gulp = require('gulp');
 var gulpif = require('gulp-if');
+var highlight = require('highlight.js');
+var layout = require('gulp-layout');
+var marked = require('marked');
 var open = require('gulp-open');
 var prettify = require('gulp-prettify');
 var rename = require("gulp-rename");
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
-var gutil = require('gulp-util');
-
-var del = require('del');
-var highlight = require('highlight.js');
-var marked = require('marked');
 var yaml = require('js-yaml');
-
 
 var renderer = new marked.Renderer();
 var COMPRESS = true;
@@ -29,32 +27,27 @@ renderer.code = function (code, language) {
   return '<pre class="highlight ' + language + '"><code>' + highlighted + '</code></pre>';
 };
 
-var readIndexYml = function() {
-  return yaml.safeLoad(fs.readFileSync('./source/index.yml', 'utf8'));
+var readGlobalYml = function() {
+  return yaml.safeLoad(fs.readFileSync('./source/global.yml', 'utf8'));
 };
 
-var getPageData = function() {
-  var config = readIndexYml();
-
-  var includes = config.includes
-        .map(function(include) { return './source/includes/' + include + '.md'; })
-        .map(function(include) { return fs.readFileSync(include, 'utf8'); })
-        .map(function(include) { return marked(include, { renderer: renderer }); });
+var getGlobalPageData = function() {
+  var config = readGlobalYml();
 
   return {
     current_page: {
       data: config
     },
+    layout: './source/layout.ejs',
     page_classes: '',
-    includes: includes,
     image_tag: function(filename, alt, className) {
-      return '<img alt="' + alt + '" class="' + className + '" src="images/' + filename + '">';
+      return '<img alt="' + alt + '" class="' + className + '" src="/images/' + filename + '">';
     },
     javascript_include_tag: function(name) {
-      return '<script src="javascripts/' + name + '.js" type="text/javascript"></script>';
+      return '<script src="/javascripts/' + name + '.js" type="text/javascript"></script>';
     },
     stylesheet_link_tag: function(name, media) {
-      return '<link href="stylesheets/' + name + '.css" rel="stylesheet" type="text/css" media="' + media + '" />';
+      return '<link href="/stylesheets/' + name + '.css" rel="stylesheet" type="text/css" media="' + media + '" />';
     },
     langs: (config.language_tabs || []).map(function(lang) {
       return typeof lang == 'string' ? lang : lang.keys.first;
@@ -75,7 +68,7 @@ gulp.task('images', function() {
 });
 
 gulp.task('js', function() {
-  var config = readIndexYml();
+  var config = readGlobalYml();
   var libs = [
     './source/javascripts/lib/_energize.js',
     './source/javascripts/lib/_jquery.js',
@@ -109,7 +102,7 @@ gulp.task('sass', function () {
 });
 
 gulp.task('highlightjs', function () {
-  var config = readIndexYml();
+  var config = readGlobalYml();
   var path = './node_modules/highlight.js/styles/' + config.highlight_theme + '.css';
   return gulp.src(path)
     .pipe(rename({ prefix: 'highlight-'}))
@@ -118,11 +111,19 @@ gulp.task('highlightjs', function () {
 });
 
 gulp.task('html', function () {
-  var data = getPageData();
-  return gulp.src('./source/*.html')
-  	.pipe(ejs(data).on('error', gutil.log))
+  var globalData = getGlobalPageData();
+  return gulp.src('./source/pages/**/index.md')
+    .pipe(frontMatter())
+    .pipe(layout(function(file) {
+      var includes = file.frontMatter.includes
+        .map(function(include) { return './source/includes/' + include + '.md'; })
+        .map(function(include) { return fs.readFileSync(include, 'utf8'); })
+        .map(function(include) { return marked(include, { renderer: renderer }); });
+
+      return assign({}, globalData, { includes: includes } );
+    }))
     .pipe(gulpif(COMPRESS, prettify({indent_size: 2})))
-  	.pipe(gulp.dest('./build'));
+    .pipe(gulp.dest('./build'));
 });
 
 gulp.task('NO_COMPRESS', function() {
@@ -133,10 +134,10 @@ gulp.task('default', ['clean', 'fonts', 'images', 'highlightjs', 'js', 'sass', '
 
 gulp.task('serve', ['NO_COMPRESS', 'default'], function() {
 
-  gulp.watch(['./source/*.html', './source/includes/**/*'], ['html']);
+  gulp.watch(['./source/layout.ejs', './source/**/*', './source/includes/**/*'], ['html']);
   gulp.watch('./source/javascripts/**/*', ['js']);
   gulp.watch('./source/stylesheets/**/*', ['sass']);
-  gulp.watch('./source/index.yml', ['highlightjs', 'js', 'html']);
+  gulp.watch('./source/global.yml', ['highlightjs', 'js', 'html']);
 
   var server = gls.static('build', 4567);
   server.start();
